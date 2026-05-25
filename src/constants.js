4256d0575362997bf51544d7e40c90fd82fa8020
Artin Melikian <artinmelikian@Artins-MacBook-Pro.local>
2026-05-25 19:06:07 +0400

    fix paid text to correct Armenian

--- src/constants.js
+++ src/constants.js
@@ -73,8 +73,8 @@ export const T = {
   STATUS_CONFIRMED: "Հաստատված",
   STATUS_DOWNLOADED:"Բեռնված",
   STATUS_PAYMENT_PENDING: "Վչարոմ սպասվոմ",
-  STATUS_PAID:     "Վչարված ե՝",
-  BTN_MARK_PAID:   "Վչարված ե՝",
+  STATUS_PAID:     "Վճարված է",
+  BTN_MARK_PAID:   "Վճարված է",
 
   // Messages
   MSG_NO_PENDING:   "Ակտիվ հայտեր չկան",
